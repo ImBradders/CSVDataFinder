6b653eb7907a4a0cadb89c1484a5f21b3b76552e
Bradley Davis <bradders.davis@gmail.com>
2020-07-20 01:11:24 +0100

    Project complete and all known bugs fixed.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -24,16 +24,153 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dialogOpenFile = new System.Windows.Forms.OpenFileDialog();
+            this.btnSelectFile = new System.Windows.Forms.Button();
+            this.lblFileToUse = new System.Windows.Forms.Label();
+            this.txtFileToUse = new System.Windows.Forms.TextBox();
+            this.lblColumnToSearchThrough = new System.Windows.Forms.Label();
+            this.txtColumnNumber = new System.Windows.Forms.TextBox();
+            this.lblTextToSearchFor = new System.Windows.Forms.Label();
+            this.txtTextToSearchFor = new System.Windows.Forms.TextBox();
+            this.lblCaseSensitive = new System.Windows.Forms.Label();
+            this.chkCaseSensitive = new System.Windows.Forms.CheckBox();
+            this.btnPerformSearch = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // dialogOpenFile
+            // 
+            this.dialogOpenFile.FileName = "dialogOpenFile";
+            // 
+            // btnSelectFile
+            // 
+            this.btnSelectFile.Location = new System.Drawing.Point(487, 25);
+            this.btnSelectFile.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnSelectFile.Name = "btnSelectFile";
+            this.btnSelectFile.Size = new System.Drawing.Size(94, 23);
+            this.btnSelectFile.TabIndex = 0;
+            this.btnSelectFile.Text = "Select File";
+            this.btnSelectFile.UseVisualStyleBackColor = true;
+            this.btnSelectFile.Click += new System.EventHandler(this.btnSelectFile_Click);
+            // 
+            // lblFileToUse
+            // 
+            this.lblFileToUse.AutoSize = true;
+            this.lblFileToUse.Location = new System.Drawing.Point(12, 7);
+            this.lblFileToUse.Name = "lblFileToUse";
+            this.lblFileToUse.Size = new System.Drawing.Size(75, 17);
+            this.lblFileToUse.TabIndex = 1;
+            this.lblFileToUse.Text = "File to Use";
+            // 
+            // txtFileToUse
+            // 
+            this.txtFileToUse.Location = new System.Drawing.Point(12, 26);
+            this.txtFileToUse.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtFileToUse.Name = "txtFileToUse";
+            this.txtFileToUse.Size = new System.Drawing.Size(469, 22);
+            this.txtFileToUse.TabIndex = 2;
+            // 
+            // lblColumnToSearchThrough
+            // 
+            this.lblColumnToSearchThrough.AutoSize = true;
+            this.lblColumnToSearchThrough.Location = new System.Drawing.Point(12, 66);
+            this.lblColumnToSearchThrough.Name = "lblColumnToSearchThrough";
+            this.lblColumnToSearchThrough.Size = new System.Drawing.Size(290, 17);
+            this.lblColumnToSearchThrough.TabIndex = 1;
+            this.lblColumnToSearchThrough.Text = "Column Number of Data to Search Through: ";
+            // 
+            // txtColumnNumber
+            // 
+            this.txtColumnNumber.Location = new System.Drawing.Point(322, 63);
+            this.txtColumnNumber.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtColumnNumber.Name = "txtColumnNumber";
+            this.txtColumnNumber.Size = new System.Drawing.Size(259, 22);
+            this.txtColumnNumber.TabIndex = 2;
+            // 
+            // lblTextToSearchFor
+            // 
+            this.lblTextToSearchFor.AutoSize = true;
+            this.lblTextToSearchFor.Location = new System.Drawing.Point(12, 103);
+            this.lblTextToSearchFor.Name = "lblTextToSearchFor";
+            this.lblTextToSearchFor.Size = new System.Drawing.Size(133, 17);
+            this.lblTextToSearchFor.TabIndex = 1;
+            this.lblTextToSearchFor.Text = "Text to Search For: ";
+            // 
+            // txtTextToSearchFor
+            // 
+            this.txtTextToSearchFor.Location = new System.Drawing.Point(152, 101);
+            this.txtTextToSearchFor.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtTextToSearchFor.Name = "txtTextToSearchFor";
+            this.txtTextToSearchFor.Size = new System.Drawing.Size(429, 22);
+            this.txtTextToSearchFor.TabIndex = 2;
+            // 
+            // lblCaseSensitive
+            // 
+            this.lblCaseSensitive.AutoSize = true;
+            this.lblCaseSensitive.Location = new System.Drawing.Point(12, 141);
+            this.lblCaseSensitive.Name = "lblCaseSensitive";
+            this.lblCaseSensitive.Size = new System.Drawing.Size(209, 17);
+            this.lblCaseSensitive.TabIndex = 1;
+            this.lblCaseSensitive.Text = "Tick for Case Sensitive Search: ";
+            // 
+            // chkCaseSensitive
+            // 
+            this.chkCaseSensitive.AutoSize = true;
+            this.chkCaseSensitive.Location = new System.Drawing.Point(228, 140);
+            this.chkCaseSensitive.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.chkCaseSensitive.Name = "chkCaseSensitive";
+            this.chkCaseSensitive.Size = new System.Drawing.Size(123, 21);
+            this.chkCaseSensitive.TabIndex = 3;
+            this.chkCaseSensitive.Text = "Case Sensitive";
+            this.chkCaseSensitive.UseVisualStyleBackColor = true;
+            // 
+            // btnPerformSearch
+            // 
+            this.btnPerformSearch.Location = new System.Drawing.Point(487, 138);
+            this.btnPerformSearch.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnPerformSearch.Name = "btnPerformSearch";
+            this.btnPerformSearch.Size = new System.Drawing.Size(94, 23);
+            this.btnPerformSearch.TabIndex = 0;
+            this.btnPerformSearch.Text = "Start";
+            this.btnPerformSearch.UseVisualStyleBackColor = true;
+            this.btnPerformSearch.Click += new System.EventHandler(this.btnPerformSearch_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(602, 171);
+            this.Controls.Add(this.btnPerformSearch);
+            this.Controls.Add(this.chkCaseSensitive);
+            this.Controls.Add(this.lblCaseSensitive);
+            this.Controls.Add(this.txtTextToSearchFor);
+            this.Controls.Add(this.lblTextToSearchFor);
+            this.Controls.Add(this.txtColumnNumber);
+            this.Controls.Add(this.lblColumnToSearchThrough);
+            this.Controls.Add(this.txtFileToUse);
+            this.Controls.Add(this.lblFileToUse);
+            this.Controls.Add(this.btnSelectFile);
+            this.Name = "Form1";
+            this.Text = "CSV Searcher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
+
+        private System.Windows.Forms.Button btnPerformSearch;
+        private System.Windows.Forms.Button btnSelectFile;
+        private System.Windows.Forms.CheckBox chkCaseSensitive;
+        private System.Windows.Forms.OpenFileDialog dialogOpenFile;
+        private System.Windows.Forms.Label lblCaseSensitive;
+        private System.Windows.Forms.Label lblColumnToSearchThrough;
+        private System.Windows.Forms.Label lblFileToUse;
+        private System.Windows.Forms.Label lblTextToSearchFor;
+        private System.Windows.Forms.TextBox txtColumnNumber;
+        private System.Windows.Forms.TextBox txtFileToUse;
+        private System.Windows.Forms.TextBox txtTextToSearchFor;
 
         #endregion
     }
--- Form1.cs
+++ Form1.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.DirectoryServices.ActiveDirectory;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +17,161 @@ namespace CSVDataFinder
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnSelectFile_Click(object sender, EventArgs e)
+        {
+            DialogResult result = dialogOpenFile.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                txtFileToUse.Text = dialogOpenFile.FileName;
+            }
+        }
+
+        private void btnPerformSearch_Click(object sender, EventArgs e)
+        {
+            if (!ValidateEntries())
+                return;
+
+            string[] fileLines = LoadFile(txtFileToUse.Text);
+            if (fileLines == null)
+                return;
+            
+            string[] dataToSearchThrough = GetColumnData(fileLines, int.Parse(txtColumnNumber.Text));
+            if (dataToSearchThrough == null)
+                return;
+            
+            int occurrences = SearchData(dataToSearchThrough, txtTextToSearchFor.Text, chkCaseSensitive.Checked);
+
+            MessageBox.Show("The text \"" + txtTextToSearchFor.Text +
+                            "\" appeared " + occurrences + " times in column " + txtColumnNumber.Text + 
+                            " in file " + txtFileToUse.Text,
+                            "Results");
+        }
+
+        private int SearchData(string[] dataToSearchThrough, string dataToFind, bool caseSensitive)
+        {
+            int count = 0;
+            foreach (string line in dataToSearchThrough)
+            {
+                if (caseSensitive)
+                {
+                    if (line.Contains(dataToFind))
+                        count++;
+                }
+                else
+                {
+                    if (line.ToLower().Contains(dataToFind.ToLower()))
+                        count++;
+                }
+            }
+
+            return count;
+        }
+
+        private string[] GetColumnData(string[] lines, int columnNumber)
+        {
+            List<string> rows = new List<string>();
+            if (!TestColNumber(lines[0], columnNumber))
+                return null;
+
+            foreach (string line in lines)
+            {
+                string[] splitLine = line.Split(',');
+                rows.Add(splitLine[columnNumber - 1]);
+            }
+
+            return rows.ToArray();
+        }
+
+        private bool TestColNumber(string data, int columnNumber)
+        {
+            string[] dataSize = data.Split(',');
+            if (columnNumber > dataSize.Length - 1)
+            {
+                MessageBox.Show("Column number entered is greater than the number of columns in the file.", 
+                    "PEBCAK");
+                return false;
+            }
+
+            return true;
+        }
+
+        private string[] LoadFile(string filePath)
+        {
+            StreamReader reader = null;
+            List<string> lines = new List<string>();
+            try
+            {
+                reader = new StreamReader(filePath);
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    lines.Add(line);
+                }
+            }
+            catch (IOException ioException)
+            {
+                MessageBox.Show("Issue while reading from file.\n\n" + ioException.Message, 
+                    "Big Scary Error Message");
+                return null;
+            }
+            finally
+            {
+                reader?.Close();
+            }
+
+            return lines.ToArray();
+        }
+
+        private bool ValidateEntries()
+        {
+            #region txtFileToUse
+            if (txtFileToUse.Text == "")
+            {
+                MessageBox.Show("Please enter the path of a file to use or select a file using the button.", "PEBCAK");
+                return false;
+            }
+            if (!File.Exists(txtFileToUse.Text))
+            {
+                MessageBox.Show("File entered does not exist.", "PEBCAK");
+                return false;
+            }
+            #endregion
+            
+            #region txtColumnNumber
+            if (txtColumnNumber.Text == "")
+            {
+                MessageBox.Show("Please enter the comlumn number containing the data that you wish to search through.", 
+                    "PEBCAK");
+                return false;
+            }
+            int colNum;
+            if (!int.TryParse(txtColumnNumber.Text, out colNum))
+            {
+                MessageBox.Show("Please ensure that a whole number is entered " +
+                                "for the column number of the data you wish to search through.", 
+                    "PEBCAK");
+                return false;
+            }
+            if (colNum < 1)
+            {
+                MessageBox.Show("Please ensure that a positive number is entered " +
+                                "for the column number of the data you wish to search through.", 
+                    "PEBCAK");
+                return false;
+            }
+            #endregion
+
+            #region txtTextToSearchFor
+            if (txtTextToSearchFor.Text == "")
+            {
+                MessageBox.Show("Please enter some data that you wish to search for.", "PEBCAK");
+                return false;
+            }
+            #endregion
+
+            return true;
         }
     }
 }
